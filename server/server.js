9d956127b6cbef6db79430e331cdf50660814296
Renny <steam_for_akk@mail.ru>
2023-10-29 16:39:47 +0300

    another6

--- server/server.js
+++ server/server.js
@@ -5,8 +5,7 @@ const bodyParser = require('body-parser');
 import fs from 'fs';
 app.use(bodyParser.json());
 app.get('/api/dataUser', (req, res) => {
-    console.log(process.cwd() + '/app/data.json');
-    res.json(JSON.parse(fs.readFileSync('/users.json').toString()).toString());
+    res.json(JSON.parse(fs.readFileSync(process.cwd()+'/users.json').toString()).toString());
 });
 app.get('/api/dataMarks', (req, res) => {
     res.json(JSON.parse(fs.readFileSync('/var/task/server/marks.json').toString()));
