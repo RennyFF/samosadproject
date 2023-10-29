0e8f91c1f708617490f69a96239f7067e7edbf28
Renny <steam_for_akk@mail.ru>
2023-10-29 15:54:37 +0300

    v001

--- server/server.js
+++ server/server.js
@@ -5,15 +5,15 @@ const bodyParser = require('body-parser');
 const fs = require("fs");
 app.use(bodyParser.json());
 app.get('/api/dataUser', (req, res) => {
-    res.json(JSON.parse(fs.readFileSync('/users.json').toString()));
+    res.json(JSON.parse(fs.readFileSync('users.json').toString()));
 });
 app.get('/api/dataMarks', (req, res) => {
-    res.json(JSON.parse(fs.readFileSync('/marks.json').toString()));
+    res.json(JSON.parse(fs.readFileSync('marks.json').toString()));
 });
 app.post('/api/saveUser', (req, res) => {
     const user = req.body;
     const jsonData = JSON.stringify(user, null, " ");
-   fs.writeFile("/users.json", jsonData, (error)=>{
+   fs.writeFile("users.json", jsonData, (error)=>{
         if (error) {
             console.error(error);
             throw error;
@@ -24,7 +24,7 @@ app.post('/api/saveUser', (req, res) => {
 app.post('/api/saveMark', (req, res) => {
     const mark = req.body;
     const jsonData = JSON.stringify(mark, null, " ");
-    fs.writeFile("/marks.json", jsonData, (error)=>{
+    fs.writeFile("marks.json", jsonData, (error)=>{
         if (error) {
             console.error(error);
             throw error;
